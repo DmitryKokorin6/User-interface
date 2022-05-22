960e658a231363e966799d71d538332da4835e65
DmitryKokorin <kokorinistra@gmail.com>
2022-05-22 18:17:14 +0300

    added HomeWork_1

---
+++ HomeWork_1/Vk/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Vk
+//
+//  Created by Дмитрий Кокорин on 21.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
