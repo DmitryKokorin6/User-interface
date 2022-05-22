5b2d5515dd2e3ad2a910d3216d16492210a58eee
DmitryKokorin <kokorinistra@gmail.com>
2022-05-22 16:49:12 +0300

    added HomeWork_1

---
+++ HomeWork_1/VK/NewViewControllerViewController.swift
@@ -0,0 +1,29 @@
+//
+//  MainViewOntrollerViewController.swift
+//  Leuire1
+//
+//  Created by Дмитрий Кокорин on 20.05.2022.
+//
+
+import UIKit
+
+class NewViewControllerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
